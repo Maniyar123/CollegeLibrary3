337df898166f21f63426d59bd8fdc80a10082f25
maniyar123 <maniyar.afeefa@gmail.com>
2020-06-08 12:05:56 +0530

    working build 8 may

--- app/src/main/java/com/afeefa/collegelibrary/HomeActivity.java
+++ app/src/main/java/com/afeefa/collegelibrary/HomeActivity.java
@@ -1,11 +1,11 @@
 package com.afeefa.collegelibrary;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
     private Button searchActivityButton;
--- app/src/main/java/com/afeefa/collegelibrary/SearchActivity.java
+++ app/src/main/java/com/afeefa/collegelibrary/SearchActivity.java
@@ -1,3 +1,4 @@
+
 package com.afeefa.collegelibrary;
 
 import android.os.Build;
@@ -6,10 +7,17 @@ import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SearchActivity extends AppCompatActivity {
     private EditText bookTextField;
@@ -27,22 +35,35 @@ public class SearchActivity extends AppCompatActivity {
         searchButton = findViewById(R.id.search_button);
         clearButton = findViewById(R.id.clear_button);
 
+        searchButton.setOnClickListener(view -> {
+            String book = bookTextField.getText().toString();
+            if (!book.isEmpty()) {
+                Log.i(SearchActivity.class.getName(),"user has entered'" + book +"'");
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String book = bookTextField.getText().toString();
-                if (!book.isEmpty()) {
-                    Log.i(SearchActivity.class.getName(),"user has entered'" + book +"'");
-                }
+                String url =
+                        String.format("%s/book/%s",getResources().getString(R.string.root_url), book);
+                Log.i(SearchActivity.class.getName(),"Navigating to " + url);
+
+                Response.Listener<JSONObject> responseListener = response -> {
+                    String title= null;
+                    try {
+                       title = response.get("title").toString();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(getApplicationContext(),title, Toast.LENGTH_LONG).show();
+                };
+
+                JsonObjectRequest searchRequest =
+                        new JsonObjectRequest(url,null, responseListener,null);
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(searchRequest);
             }
         });
-        clearButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
+        clearButton.setOnClickListener(view -> {
                 bookTextField.setText(R.string.empty_string);
-                Log.i(SearchActivity.class.getName(), "Book Text field has been cleard.");
-            }
+                Log.i(SearchActivity.class.getName(), "Book Text field has been cleared.");
         });
 
     }
